ebba478d6ade0f6c7c864f14827a927a455cf208
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-10-18 10:52:24 +0300

    add file

---
+++ app.rb
@@ -0,0 +1,11 @@
+# app.rb puts takes strings and breaks after the line.
+puts "Hello world!"
+
+#print doesnt break line after string.
+print "Pass this test, please."
+
+#p method good for arrays/ non strings.
+p [1, 2, 3]
+
+puts "Hello World!\n"
+
